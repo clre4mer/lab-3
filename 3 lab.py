d597b398fd6a6213356fc8fa02d9033d83f4dd10
clre4mer <113285084+clre4mer@users.noreply.github.com>
2022-10-09 13:02:10 +0500

    коментарии + интерфейс

--- 3 lab.py
+++ 3 lab.py
@@ -1,16 +1,57 @@
-alfavit =  'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзиклмнопрстуфхцчшщъьэюяабвгдеёжзиклмнопрстуфхцчшщъьэюя'
-text = input("Введите текст для шифрования:\n")
-k = int(input("Введите отступ для шифрования:\n"))
-if k>=33:
-    while k >=33:
-        k = k - 33;
-rez =''
-for i in text:
-    m1 = alfavit.find(i)
-    m2 = m1 + k
-    if i in alfavit:
-        rez = rez + alfavit[m2]
+class NumError(Exception): #создаём исключение
+    def __init__(self, text):
+        self.txt = text
+
+alfavit =  'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзиклмнопрстуфхцчшщъьэюяабвгдеёжзиклмнопрстуфхцчшщъьэюя'#русский алфавит
+alfavitEng = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'#английский алфавит
+v=1
+while v!=0:#цикл ввода с обработкой ошибок
+    try:
+        n=int(input(f'На каком языке будет зашифрованное сообщение?\n 1- Русский\n 2 - Английский\n'))
+        if n>2 or n<1:
+            raise NumError('Введённое число не соответствует вариантам выбора')
+
+    except ValueError:
+        print('Ошибка ввода данных, нужно ввести число')
+        v=1
+    except NumError as s:
+        print('Ошибка ввода данных')
+        print (s)
+        v=1
     else:
-        rez = rez + i
+        v=0
+v=1
+while v!=0:#цикл ввода с обработкой ошибок
+    try:
+        k = int(input("Введите отступ для шифрования:\n"))
+    except ValueError:
+        print('Ошибка ввода данных, нужно ввести число')
+        v=1
+    else:
+        v=0
+text = input('Введите текст для шифрования\n')
+rez =''
+if n==1:#ветвление в зависимости от выбранного языка
+    if k>=33:
+        while k >=33:
+            k = k- 33
+    for i in text:
+        m1 = alfavit.find(i)
+        m2 = m1 + k
+        if i in alfavit:
+            rez = rez + alfavit[m2]
+        else:
+            rez = rez + i
+else:
+    if k>=26:
+        while k>=26:
+            k = k - 26
+    for i in text:
+        m1 = alfavitEng.find(i)
+        m2 = m1 + k
+        if i in alfavitEng:
+            rez = rez + alfavitEng[m2]
+        else:
+            rez = rez + i
 print (rez)
 
